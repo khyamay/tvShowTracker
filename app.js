57f7774e745560a6534b0f13420af16f241bef9d
Khemraj Thapa <khyamay@gmail.com>
2014-06-06 23:53:30 +0100

    added routes and views

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-.config(function(){
-
+.config(function($locationProvider){
+	$locationProvider.html5Mode(true);
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/home.html',
+		controller: 'MainCtrl'
+	})
+	.when('/shows/:id', {
+		templateUrl: 'views/details.html',
+		controller: 'DetailCtrl'
+	})
+	.when('/login', {
+		templateUrl: 'views/login.html',
+		controller: 'SingupCtrl'
+	})
+	.when('/add', {
+		templateUrl: 'views/add.html',
+		controller: 'AddCtrl'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
 });
