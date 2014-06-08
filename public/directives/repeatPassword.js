365fbd4d59189af447dfbf911c256e21217b0d01
Khemraj Thapa <khyamay@gmail.com>
2014-06-08 01:21:54 +0100

    fixing typoes

--- public/directives/repeatPassword.js
+++ public/directives/repeatPassword.js
@@ -1,24 +1,22 @@
 angular.module('MyApp')
-	.directive('repeatPassword', function(){
-		return
-		{
-			require: 'ngModel',
-			link: function(scope, elem, attrs, ctrl){
-				var otherInput = elem.inheritedData('$formController') [attrs.repeatPassword];
+  .directive('repeatPassword', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, elem, attrs, ctrl) {
+        var otherInput = elem.inheritedData("$formController")[attrs.repeatPassword];
 
-				ctrl.$parsers.push(function(value){
-					if(value === otherInput.$viewValue) {
-						ctrl.$setValidity('repeat', true);
-						return value;
-					}
+        ctrl.$parsers.push(function(value) {
+          if (value === otherInput.$viewValue) {
+            ctrl.$setValidity('repeat', true);
+            return value;
+          }
+          ctrl.$setValidity('repeat', false);
+        });
 
-					ctrl.$setValidity('repeat', false)
-				});
-
-				otherInput.$parsers.push(function(value){
-					ctrl.$setValidity('repeat', value === ctrl.$viewValue);
-					return value;
-				});
-			}
-		};
-	});
+        otherInput.$parsers.push(function(value) {
+          ctrl.$setValidity('repeat', value === ctrl.$viewValue);
+          return value;
+        });
+      }
+    };
+  });
